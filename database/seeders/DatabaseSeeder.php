3c652d8ba5d611c80425f5c1e19a92c517972dd8
damar911 <dimasdaludamara@gmail.com>
2026-04-26 20:47:24 +0700

    Tambah Migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,6 +33,18 @@ class DatabaseSeeder extends Seeder
             'name'  =>  'Entertaiment' , 
             'slug'  =>  'entertaiment' , 
         ]); 
+        $category3 = \App\Models\Category::firstOrCreate([ 
+            'name'  =>  'Creative Tect & UI/UX' , 
+            'slug'  =>  'creative tect & ui/ux' , 
+        ]); 
+        $category4 = \App\Models\Category::firstOrCreate([ 
+            'name'  =>  'Programing & Development' , 
+            'slug'  =>  'programing & development' , 
+        ]); 
+        $category5 = \App\Models\Category::firstOrCreate([ 
+            'name'  =>  'Data & Analytics' , 
+            'slug'  =>  'data & analytics' , 
+        ]); 
 
         \App\Models\Event::create([ 
             'category_id'  => $category2->id, 
@@ -69,5 +81,36 @@ class DatabaseSeeder extends Seeder
             'stock'  =>  100 , 
             'poster_path'  =>  'posters/event-3.png' , 
         ]);
+        \App\Models\Event::create([ 
+            'category_id'  => $category3->id, 
+            'title'  =>  'UI/UX Masterclass' , 
+            'description'  =>  'Workshop intensif tentang desain antarmuka dan pengalaman pengguna, cocok untuk mahasiswa yang tertarik pada front-end dan desain sistem informasi.' , 
+            'date'  =>  '2026-05-01 13:00:00' , 
+            'location'  =>  'Cinema Unit 6' , 
+            'price'  =>  50000 , 
+            'stock'  =>  100 , 
+            'poster_path'  =>  'posters/event-4.png' , 
+        ]);
+        \App\Models\Event::create([ 
+            'category_id'  => $category4->id, 
+            'title'  =>  'Code Sprint Challenge' , 
+            'description'  =>  'Kompetisi membangun aplikasi web/mobile dalam waktu terbatas, fokus pada problem solving dan teamwork.' , 
+            'date'  =>  '2026-05-01 13:00:00' , 
+            'location'  =>  'Cinema Unit 6' , 
+            'price'  =>  50000 , 
+            'stock'  =>  100 , 
+            'poster_path'  =>  'posters/event-5.png' , 
+        ]);
+        \App\Models\Event::create([ 
+            'category_id'  => $category5->id, 
+            'title'  =>  'Data Visualization Hackathon' , 
+            'description'  =>  'Tantangan membuat dashboard interaktif dengan tools seperti Tableau atau Power BI.' , 
+            'date'  =>  '2026-05-01 13:00:00' , 
+            'location'  =>  'Cinema Unit 6' , 
+            'price'  =>  50000 , 
+            'stock'  =>  100 , 
+            'poster_path'  =>  'posters/event-6.png' , 
+        ]);
+
     }
 }
